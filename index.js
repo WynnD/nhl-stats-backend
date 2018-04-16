7e0398e1191d67437a90f81fa28fef4beaa0f4b0
Wynn Drahorad <wynn.drahorad@gmail.com>
2018-04-16 16:22:10 -0500

    add error logging code

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const config = require('config');
 
 const dbConfig = config.get("dbConfig");
 
-if (dbConfig.user) {
-  mongoose.connect(`mongodb+srv://${dbConfig.user.username}:${dbConfig.user.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`);
-} else {
-  mongoose.connect(`mongodb://${dbConfig.host}:${dbConfig.port}`);
+try {
+  if (dbConfig.user) {
+    await mongoose.connect(`mongodb+srv://${dbConfig.user.username}:${dbConfig.user.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.dbName}`);
+  } else {
+    await mongoose.connect(`mongodb://${dbConfig.host}:${dbConfig.port}`);
+  }
+} catch (e) {
+  console.error(e);
 }
